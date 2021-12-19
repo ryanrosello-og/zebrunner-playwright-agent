06479b27401feb4ee156ff3dc697ccd72a079886
Ryan.Rosello <ryan.r@autopilothq.com>
2021-12-19 16:01:42 +1000

    github specific config

---
+++ playwright_github.config.ts
@@ -0,0 +1,29 @@
+// playwright.config.ts
+import {PlaywrightTestConfig, devices} from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'webkit',
+      use: {...devices['Desktop Safari']},
+    },
+  ],
+  reporter: [
+    [
+      './src/lib/zebReporter.js',
+      {
+        reporterBaseUrl: 'https://default.zebrunner.com',
+        projectKey: 'DEF',
+        enabled: false,
+        concurrentTasks: 19,
+      },
+    ],
+  ],
+};
+export default config;
